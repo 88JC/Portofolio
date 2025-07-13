afac4c67d107e9ba251498acd8b642308f208493
88JC <chanjecky888@gmail.com>
2025-07-13 10:41:34 +0700

    Update project metadata & changed URL and updated last modified date.

--- src/lib/data/other-projects.ts
+++ src/lib/data/other-projects.ts
@@ -5,9 +5,9 @@ export const otherProjects = [
     id: 1001,
     name: "Soulvib Bot Website",
     description: "Soulvibe Music, The ultimate music bot for Discord, This is the of the available free bots. Bot with support for sources such as Spotify, Soundcloud and more!",
-    html_url: `https://github.com/${config.github.username}/Soulvib-Bot`,
-    homepage: "https://soulvib-bot.com",
-    updated_at: "2024-01-15T10:30:00Z",
+    html_url: `https://github.com/Soulvibe-Bot`,
+    homepage: "https://soulvibe-bot.com",
+    updated_at: "2025-07-13T10:30:00Z",
     cover: "/img/project/soulvibe.png"
   },
 ]; 
